7159f7f7f9e8228930c3d343ebc70a3f93893615
Alberto <betim009@gmail.com>
2023-11-20 15:15:26 -0300

    feat: Validando login com o context e começando página Cadastro

--- src/pages/login-2/index.jsx
+++ src/pages/login-2/index.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react"
+import Context from "../../context/Context";
+
 
 function Login2() {
     const navigate = useNavigate();
+
+    const { users } = useContext(Context);
 
     const [formLogin, setFormLogin] = useState({
         email: '',
@@ -13,7 +18,16 @@ function Login2() {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        navigate("/users");
+        // Verificar se existe esse email no Context
+        const { email, password } = formLogin;
+        const userExists = users.some((user) => user.email === email && user.password === password);
+
+        if (userExists) {
+            // Navigate to /users
+            navigate("/users");
+        } else {
+            alert("Usuário não cadastrado");
+        }
     };
 
     const handleChange = ({ target }) => {
